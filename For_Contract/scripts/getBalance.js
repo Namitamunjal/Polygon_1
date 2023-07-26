bada256ea27837e9bcf441cc4018afb27e61840e
Namitamunjal <70011579+Namitamunjal@users.noreply.github.com>
2023-07-26 11:09:52 +0530

    Create getBalance.js

---
+++ For_Contract/scripts/getBalance.js
@@ -0,0 +1,25 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
+
+const tokenAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+const tokenABI = tokenContractJSON.abi;
+
+async function main() {
+
+    const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
+
+    console.log("You now have: " + await token.balanceOf(walletAddress) + " tokens");
+  }
+  
+  // We recommend this pattern to be able to use async/await everywhere
+  // and properly handle errors.
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
